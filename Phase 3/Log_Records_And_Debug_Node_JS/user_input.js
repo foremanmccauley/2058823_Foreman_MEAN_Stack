1d767c7ec515b9992877a279194089ac637aa2b8
foremanmccauley <fmccauley_205@yahoo.com>
2021-08-25 14:05:09 -0500

    Completed Logger_and_Debug project

---
+++ Phase 3/Log_Records_And_Debug_Node_JS/user_input.js
@@ -0,0 +1,75 @@
+let obj = require("readline");
+const fs = require('fs');
+const path = './all_users.json';
+
+var q1 = obj.createInterface({
+    input:process.stdin,        
+    output:process.stdout       
+});
+
+addUser();
+
+function addUser() {
+    q1.question("Enter your first name: ",(fName)=> {
+        debugger;
+        q1.question("Enter your last name: ",(lName)=> {
+            debugger;
+            q1.question("Enter your gender: ",(gender)=> {
+                debugger;
+                q1.question("Enter your email: ",(email)=> {
+                    debugger;
+                    let currentDate = new Date();
+                    // current date
+                    let date = ("0" + currentDate.getDate()).slice(-2);
+
+                    // current month
+                    let month = ("0" + (currentDate.getMonth() + 1)).slice(-2);
+
+                    // current year
+                    let year = currentDate.getFullYear();
+
+                    // current hours
+                    let hours = currentDate.getHours();
+
+                    // current minutes
+                    let minutes = currentDate.getMinutes();
+
+                    // current seconds
+                    let seconds = currentDate.getSeconds();
+                    let newUser = {
+                        firstName: fName,
+                        lastName: lName,
+                        gender: gender,
+                        email: email,
+                        storingDate: month + " " + date + " " + year,
+                        storingTime: hours + ":" + minutes + ":" + seconds
+                    }
+                    
+                    
+
+                    if (fs.existsSync(path)) {
+                        fs.readFile('all_users.json', (err, data) => {
+                            if (err) throw err;
+                            let all_users = JSON.parse(data);
+                            all_users.push(newUser);
+                            let all_users_string = JSON.stringify(all_users);
+                            fs.writeFileSync('all_users.json', all_users_string);
+                        })
+                    }
+                    else {
+                        let all_users = [];
+                        all_users.push(newUser);
+                        let all_users_string = JSON.stringify(all_users);
+                        fs.writeFileSync('all_users.json', all_users_string);
+                    }
+
+                    
+                    debugger;
+                    
+
+                    q1.close();
+                })
+            })
+        })
+    })
+}
